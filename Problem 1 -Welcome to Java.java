0c7831b9ec79d0be7f61421826eea672cd2c38e9
VIGNESH S <83010116+Vickydeveloper@users.noreply.github.com>
2023-02-12 15:45:24 +0530

    Add files via upload

---
+++ Problem 1 -Welcome to Java.java
@@ -0,0 +1,9 @@
+//Give the name "Solution.java" to this file.
+
+public class Solution {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
